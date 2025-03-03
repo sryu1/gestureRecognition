50a897316f4ac182e5ddad3c0fdee98f64c7f3b4
Theo Dela Cruz <153193173+tdela12@users.noreply.github.com>
2025-03-03 19:49:31 +1000

    Set up gesture_recog.py

--- gesture_recog.py
+++ gesture_recog.py
@@ -1,1 +1,15 @@
+#Import the necessary packages
 
+#Initialize video capture in a variable
+
+#Enter the infinite while loop here
+
+    #Read in a frame of the webcam feed and store it
+
+    #Apply image processing
+
+    #Identify and store hand landmarks
+
+    #Pass data into the ml model
+
+    #logic to handle 
